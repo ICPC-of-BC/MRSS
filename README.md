546ead9d6092c3d857099d62cccbf34ebce5c180
stkddks <stkddks@naver.com>
2019-09-16 05:40:18 +0000

    add third

--- README.md
+++ README.md
@@ -1,41 +1,119 @@
-[//]: # (SPDX-License-Identifier: CC-BY-4.0)
+## MRSS (Medical Record Sharing Service)
+### **Hyperledger Fabric을 이용한 의료기록 공유 시스템 개발**
 
-## Hyperledger Fabric Samples
+* * *
 
-Please visit the [installation instructions](http://hyperledger-fabric.readthedocs.io/en/latest/install.html)
-to ensure you have the correct prerequisites installed. Please use the
-version of the documentation that matches the version of the software you
-intend to use to ensure alignment.
+**1. 팀 소개**
+* 안종민
+* 이상아
+* 이규환
+* 전성원
+* 명진우
 
-## Download Binaries and Docker Images
+**2. 프로젝트 개요**
+ - 병원들끼리의 
 
-The installation instructions will utilize `scripts/bootstrap.sh` (available in the fabric repository)
-script to download all of the requisite Hyperledger Fabric binaries and docker
-images, and tag the images with the 'latest' tag. Optionally,
-specify a version for fabric, fabric-ca and thirdparty images. If versions
-are not passed, the latest available versions will be downloaded.
-
-The script will also clone fabric-samples repository using the version tag that
-is aligned with the Fabric version.
-
-You can also download the script and execute locally:
-
-```bash
-# Fetch bootstrap.sh from fabric repository using
-curl -sS https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh -o ./scripts/bootstrap.sh
-# Change file mode to executable
-chmod +x ./scripts/bootstrap.sh
-# Download binaries and docker images
-./scripts/bootstrap.sh [version] [ca version] [thirdparty_version]
+**Contract**
+```
+● 환자 정보 등록 및 요청
+  ○ 이름
+  ○ 주민번호
+  ○ 성별
+● 환자 정보 수정 및 삭제
+● 의료기록 입력
+  ○ 수술기록
+  ○ 진찰기록
 ```
 
-### Continuous Integration
+**UI/UX**
 
-Please have a look at [Continuous Integration Process](docs/fabric-samples-ci.md)
 
-## License <a name="license"></a>
+![MRSS구조](https://user-images.githubusercontent.com/49246977/64236333-273c7280-cf35-11e9-9fb8-f4c5cc81d94b.png)
 
-Hyperledger Project source code files are made available under the Apache
-License, Version 2.0 (Apache-2.0), located in the [LICENSE](LICENSE) file.
-Hyperledger Project documentation files are made available under the Creative
-Commons Attribution 4.0 International License (CC-BY-4.0), available at http://creativecommons.org/licenses/by/4.0/.
+* * *
+
+## PROGRESS
+
+* * *
+
+### ~2019.09.11 Modifying 'fabcar'file to 'usermanager'
+
+**Edit Chaincode**
+```
+cd ~/fabric-samples/chaincode/
+cp -r fabcar usermanager
+mv usermanager/go/fabcar.go usermanager/go/usermanager.go
+```
+> rename **'fabcar'** to **'usermanager'**
+
+**Change usermanager.go**
+```
+nano usermanager/go/usermanager.go
+```
+> change inside file's function and variables name
+like this:
+* Car → Person
+* car → person
+* Make → Firstname
+* Model → Lastname
+* Colour → Email
+* Owner → Phone
+* make → firstname
+* model → lastname
+* colour → email
+* owner → phone
+* CAR → PERSON
+
+**Edit usermanager**
+```
+cd ..
+cp -r fabcar usermanager
+cd usermanager
+```
+> modify file's name (fabcar → usermanager), command: 'cp -r'
+
+**change startFabric.sh**
+```
+nano startFabric.sh
+```
+> change whole word 'fabcar' to 'usermanager'(fabcar → usermanager)
+
+**change query.js**
+```
+nano javascript/query.js
+```
+> changed:
+- line 41: const result = await contract.evaluateTransaction('queryUser', 'USER5'); #remark
+- line 41: const result = await contract.evaluateTransaction('queryAllUsers'); #add
+- fabcar → User
+- CAR → PERSON
+
+**change invoke.js**
+```
+nano javascript/invoke.js
+```
+> modified:
+- await contract.submitTransaction('createUser', 'USER5', 'Ahn', 'jongmin', 'ggg@ggg.com', '010-7777-7777'); #code
+- fabcar → usermanager
+- Car → Person
+- CAR → PERSON
+- Owner → Phone
+
+**RUN**
+```
+cd javascript
+npm install
+cd ..
+./startFabcar.sh
+
+cd javascript
+node enrollAdmin.js
+node registerUser.js
+node query.js
+node invoke.js
+```
+> **!!!CAUTION!!!**
+You Must Follow This Order
+
+* * *
+
--- chaincode/MRSSsample/go/MRSSsample.go
+++ chaincode/MRSSsample/go/MRSSsample.go
@@ -29,235 +29,228 @@ package main
  * 2 specific Hyperledger Fabric specific libraries for Smart Contracts
  */
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"strconv"
+        "bytes"
+        "encoding/json"
+        "fmt"
+        "strconv"
 
-	"github.com/hyperledger/fabric/core/chaincode/shim"
-	sc "github.com/hyperledger/fabric/protos/peer"
+        "github.com/hyperledger/fabric/core/chaincode/shim"
+        sc "github.com/hyperledger/fabric/protos/peer"
 )
 
 // Define the Smart Contract structure
 type SmartContract struct {
 }
 
-// Define the Person structure, with 4 properties.  Structure tags are used by encoding/json library
+// Define the person structure, with 4 properties.  Structure tags are used by encoding/json library
 type Person struct {
-	Name   string `json:"name"`
-	Number  string `json:"number"`
-	Sex string `json:"sex"`
+        Name   string `json:"name"`
+        Number  string `json:"number"`
+        Sex string `json:"sex"`
+        DorO string `json:"doro"`
+        PM string `json:"pm"`
 }
 
-type Medical struct {
-	Operation string `json:"operation"`
-	Op_Medication string `json:"op_medication"`
-	Diagnosis string `json:"diagnosis"`
-	Diag_Medication string `json:"diag_medication"`
-}
+//type Medical struct {
+//      DorO string `json:"doro"`
+//      PM string `json:"pm"`
+//}
 
 /*
- * The Init method is called when the Smart Contract "fabPerson" is instantiated by the blockchain network
+ * The Init method is called when the Smart Contract "MRSSsample" is instantiated by the blockchain network
  * Best practice is to have any Ledger initialization in separate function -- see initLedger()
  */
 func (s *SmartContract) Init(APIstub shim.ChaincodeStubInterface) sc.Response {
-	return shim.Success(nil)
+        return shim.Success(nil)
 }
 
 /*
- * The Invoke method is called as a result of an application request to run the Smart Contract "fabPerson"
+ * The Invoke method is called as a result of an application request to run the Smart Contract "MRSSsample"
  * The calling application program has also specified the particular smart contract function to be called, with arguments
  */
 func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response {
 
-	// Retrieve the requested Smart Contract function and arguments
-	function, args := APIstub.GetFunctionAndParameters()
-	// Route to the appropriate handler function to interact with the ledger appropriately
-	if function == "queryPerson" {
-		return s.queryPerson(APIstub, args)
-	} else if function == "initLedger" {
-		return s.initLedger(APIstub)
-	} else if function == "createPerson" {
-		return s.createPerson(APIstub, args)
-	} else if function == "queryAllPersons" {
-		return s.queryAllPersons(APIstub)
-
-	} else if function == "queryMedical" {
+        // Retrieve the requested Smart Contract function and arguments
+        function, args := APIstub.GetFunctionAndParameters()
+        // Route to the appropriate handler function to interact with the ledger appropriately
+        if function == "queryPerson" {
                 return s.queryPerson(APIstub, args)
-	} else if function == "initLedgerMedical" {
+        } else if function == "initLedger" {
                 return s.initLedger(APIstub)
-	} else if function == "createMedical" {
+        } else if function == "createPerson" {
                 return s.createPerson(APIstub, args)
-	}
+        } else if function == "queryAllPersons" {
+                return s.queryAllPersons(APIstub)
+//      } else if function == "changePersonOwner" {
+//              return s.changePersonOwner(APIstub, args)
+        } else if function == "createMedical" {
+                return s.createMedical(APIstub, args)
+        }
 
-	return shim.Error("Invalid Smart Contract function name.")
+        return shim.Error("Invalid Smart Contract function name.")
 }
 
 func (s *SmartContract) queryPerson(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
-
-	if len(args) != 1 {
-		return shim.Error("Incorrect number of arguments. Expecting 1")
-	}
-
-	PersonAsBytes, _ := APIstub.GetState(args[0])
-	return shim.Success(PersonAsBytes)
-}
-/*
-func (s *SmartContract) queryMedical(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
 
         if len(args) != 1 {
                 return shim.Error("Incorrect number of arguments. Expecting 1")
         }
 
-        PersonAsBytes, _ := APIstub.GetState(args[0])
-        return shim.Success(PersonAsBytes)
+        personAsBytes, _ := APIstub.GetState(args[0])
+//      fmt.Println( string( personAsBytes ) )
+
+        return shim.Success( personAsBytes )
 }
 
-*/
 func (s *SmartContract) initLedger(APIstub shim.ChaincodeStubInterface) sc.Response {
-	Persons := []Person{
-		Person{Name: "Toyota", Number: "111111-1111111", Sex: "blue"},
-		Person{Name: "Ford", Number: "222222-2222222", Sex: "red"},
-		Person{Name: "Hyundai", Number: "333333-3333333", Sex: "green"},
-		Person{Name: "Volkswagen", Number: "444444-4444444", Sex: "yellow"},
-		Person{Name: "Tesla", Number: "111112-111111", Sex: "black"},
-		Person{Name: "Peugeot", Number: "111112-111112", Sex: "purple"},
-		Person{Name: "Chery", Number: "222222-2222223", Sex: "white"},
-		Person{Name: "Fiat", Number: "222222-2222244", Sex: "violet"},
-		Person{Name: "Tata", Number: "222222-2222555", Sex: "indigo"},
-		Person{Name: "Holden", Number: "881212-2111111", Sex: "brown"},
-	}
-
-	i := 0
-	for i < len(Persons) {
-		fmt.Println("i is ", i)
-		PersonAsBytes, _ := json.Marshal(Persons[i])
-		APIstub.PutState("Person"+strconv.Itoa(i), PersonAsBytes)
-		fmt.Println("Added", Persons[i])
-		i = i + 1
-	}
-
-	return shim.Success(nil)
-}
-/*
-func (s *SmartContract) initLedgerMedical(APIstub shim.ChaincodeStubInterface) sc.Response {
-        Medicals := []Medical{
-                Medical{Operation: "Cancer surgery", Op_Medication: "morphine", Diagnosis: "cold", Diag_Medication: "Brufen"},
-                Medical{Operation: "Cancer surgery", Op_Medication: "morphine", Diagnosis: "cold", Diag_Medication: "Brufen"},
-                Medical{Operation: "Cancer surgery", Op_Medication: "morphine", Diagnosis: "cold", Diag_Medication: "Brufen"},
-                Medical{Operation: "Cancer surgery", Op_Medication: "morphine", Diagnosis: "cold", Diag_Medication: "Brufen"},
+        persons := []Person{
+                Person{Name: "LeeSA", Number: "961025", Sex: "F", DorO: "D", PM: "Advil, Anacin, Bayer, Bufferin"},
+                Person{Name: "MyungJW", Number: "940000", Sex: "M", DorO: "D", PM: "Halls, Robitussin, Sucrets, Vicks"},
+                Person{Name: "LeeGH", Number: "950000", Sex: "M", DorO: "O", PM: "GasX, Maalox, Rolaid, Tums"},
         }
 
         i := 0
-        for i < len(Medicals) {
+        for i < len(persons) {
                 fmt.Println("i is ", i)
-                PersonAsBytes, _ := json.Marshal(Medicals[i])
-                APIstub.PutState("Person"+strconv.Itoa(i), PersonAsBytes)
-                fmt.Println("Added", Medicals[i])
+                personAsBytes, _ := json.Marshal( persons[i] )
+                APIstub.PutState("PERSON"+strconv.Itoa(i), personAsBytes )
+                fmt.Println( "Added", persons[i] )
                 i = i + 1
         }
 
+//      medicals := []Medical {
+//                Medical{ DorO: "D", PM: "Advil, Anacin, Bayer, Bufferin"},
+//                Medical{ DorO: "D", PM: "Halls, Robitussin, Sucrets, Vicks"},
+//                Medical{ DorO: "O", PM: "GasX, Maalox, Rolaid, Tums"},
+//        }
+
+//      j := 0
+//      for i < len( medicals ) {
+//              fmt.Println( "i is ", j )
+//              medicalAsBytes, _ := json.Marshal( medicals[j] )
+//              APIstub.PutState( "PERSON"+strconv.Itoa(j), medicalAsBytes )
+//              fmt.Println( "Added", medicals[j] )
+//              j = j+1
+//      }
+
         return shim.Success(nil)
 }
-*/
+
 func (s *SmartContract) createPerson(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
 
-	if len(args) != 4 {
-		return shim.Error("Incorrect number of arguments. Expecting 5")
-	}
-
-	var Person = Person{Name: args[1], Number: args[2], Sex: args[3]}
-
-	PersonAsBytes, _ := json.Marshal(Person)
-	APIstub.PutState(args[0], PersonAsBytes)
-
-	return shim.Success(nil)
-}
-
-/*
-func (s *SmartContract) createMedical(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
-
         if len(args) != 4 {
-                return shim.Error("Incorrect number of arguments. Expecting 5")
+                return shim.Error("Incorrect number of arguments. Expecting 4")
         }
 
-        //var Person = Person{Name: args[1], Number: args[2], Sex: args[3]}
-        var Medical = Medical{Operation: args[1], Op_Medication: args[2], Diagnosis: args[3], Diag_Medication: args[4]}
-        MedicalAsBytes, _ := json.Marshal(Medical)
-        APIstub.PutState(args[0], MedicalAsBytes)
+        var person = Person{Name: args[1], Number: args[2], Sex: args[3]}
+
+        personAsBytes, _ := json.Marshal( person )
+        APIstub.PutState(args[0], personAsBytes)
 
         return shim.Success(nil)
 }
-*/
-
 
 func (s *SmartContract) queryAllPersons(APIstub shim.ChaincodeStubInterface) sc.Response {
 
-	startKey := "Person0"
-	endKey := "Person999"
+        startKey := "PERSON0"
+        endKey := "PERSON999"
 
-	resultsIterator, err := APIstub.GetStateByRange(startKey, endKey)
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-	defer resultsIterator.Close()
+        resultsIterator, err := APIstub.GetStateByRange(startKey, endKey)
+        if err != nil {
+                return shim.Error(err.Error())
+        }
+        defer resultsIterator.Close()
 
-	// buffer is a JSON array containing QueryResults
-	var buffer bytes.Buffer
-	buffer.WriteString("[")
+        // buffer is a JSON array containing QueryResults
+        var buffer bytes.Buffer
+        buffer.WriteString("[")
 
-	bArrayMemberAlreadyWritten := false
-	for resultsIterator.HasNext() {
-		queryResponse, err := resultsIterator.Next()
-		if err != nil {
-			return shim.Error(err.Error())
-		}
-		// Add a comma before array members, suppress it for the first array member
-		if bArrayMemberAlreadyWritten == true {
-			buffer.WriteString(",")
-		}
-		buffer.WriteString("{\"Key\":")
-		buffer.WriteString("\"")
-		buffer.WriteString(queryResponse.Key)
-		buffer.WriteString("\"")
+        bArrayMemberAlreadyWritten := false
+        for resultsIterator.HasNext() {
+                queryResponse, err := resultsIterator.Next()
+                if err != nil {
+                        return shim.Error(err.Error())
+                }
+                // Add a comma before array members, suppress it for the first array member
+                if bArrayMemberAlreadyWritten == true {
+                        buffer.WriteString(",")
+                }
+                buffer.WriteString("{\"Key\":")
+                buffer.WriteString("\"")
+                buffer.WriteString(queryResponse.Key)
+                buffer.WriteString("\"")
 
-		buffer.WriteString(", \"Record\":")
-		// Record is a JSON object, so we write as-is
-		buffer.WriteString(string(queryResponse.Value))
-		buffer.WriteString("}")
-		bArrayMemberAlreadyWritten = true
-	}
-	buffer.WriteString("]")
+                buffer.WriteString(", \"Record\":")
+                // Record is a JSON object, so we write as-is
+                buffer.WriteString(string(queryResponse.Value))
+                buffer.WriteString("}")
+                bArrayMemberAlreadyWritten = true
+        }
+        buffer.WriteString("]")
 
-	fmt.Printf("- queryAllPersons:\n%s\n", buffer.String())
+        fmt.Printf("- queryAllPersons:\n%s\n", buffer.String())
 
-	return shim.Success(buffer.Bytes())
+        return shim.Success(buffer.Bytes())
 }
 /*
 func (s *SmartContract) changePersonOwner(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
 
-	if len(args) != 2 {
-		return shim.Error("Incorrect number of arguments. Expecting 2")
-	}
+        if len(args) != 2 {
+                return shim.Error("Incorrect number of arguments. Expecting 2")
+        }
 
-	PersonAsBytes, _ := APIstub.GetState(args[0])
-	Person := Person{}
+        personAsBytes, _ := APIstub.GetState(args[0])
+        person := Person{}
 
-	json.Unmarshal(PersonAsBytes, &Person)
-	Person.Owner = args[1]
+        json.Unmarshal(personAsBytes, &person)
+        person.Owner = args[1]
 
-	PersonAsBytes, _ = json.Marshal(Person)
-	APIstub.PutState(args[0], PersonAsBytes)
+        personAsBytes, _ = json.Marshal( person )
+        APIstub.PutState(args[0], personAsBytes)
 
-	return shim.Success(nil)
+        return shim.Success(nil)
 }
 */
+
+func (s *SmartContract) createMedical( APIstub shim.ChaincodeStubInterface, args []string ) sc.Response {
+        tmpAsBytes, _:= APIstub.GetState(args[0])
+
+        if tmpAsBytes == nil {
+                return shim.Error( "No data" )
+        }
+
+        if len( args ) != 3 {
+                return shim.Error( "Incorrect number of arguments. Expection 3" )
+        }
+
+        var person = Person{ DorO: args[1], PM: args[2] }
+
+        personAsBytes, _ := json.Marshal( person )
+        APIstub.PutState(args[0], personAsBytes)
+
+        return shim.Success(nil)
+}
+
+//      personAsBytes, _:= APIstub.GetState( args[0] )
+//      person := Person{}
+
+//      json.Unmarshal( personAsBytes, &person )
+//      person.DorO = args[1]
+//      person.PM = args[2]
+
+//      personAsBytes, _ = json.Marshal( person )
+//      APIstub.PutState( args[0], personAsBytes )
+
+//      return shim.Success( nil )
+//}
+
+
 // The main function is only relevant in unit test mode. Only included here for completeness.
 func main() {
 
-	// Create a new Smart Contract
-	err := shim.Start(new(SmartContract))
-	if err != nil {
-		fmt.Printf("Error creating new Smart Contract: %s", err)
-	}
+        // Create a new Smart Contract
+        err := shim.Start(new(SmartContract))
+        if err != nil {
+                fmt.Printf("Error creating new Smart Contract: %s", err)
+        }
 }
